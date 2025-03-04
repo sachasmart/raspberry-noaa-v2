175105feed3b7b27bbb44f16b16d858f00b4d047
Sacha <sacha.smart@gmail.com>
2025-03-04 13:15:26 -0500

    add mqtt to schedule

--- scripts/schedule_captures.sh
+++ scripts/schedule_captures.sh
@@ -112,6 +112,20 @@ while [ "$(date --date="@${end_epoch_time}" +"%s")" -le "${END_TIME_MS}" ]; do
 
       # update database with scheduled pass
       $SQLITE3 $DB_FILE "INSERT OR REPLACE INTO predict_passes (sat_name,pass_start,pass_end,max_elev,is_active,pass_start_azimuth,azimuth_at_max,direction,at_job_id) VALUES (\"${OBJ_NAME}\",$start_epoch_time,$end_epoch_time,$max_elev,1,$starting_azimuth,$azimuth_at_max,'$direction',$at_job_id);"
+      # Emit mqtt message here
+      log "Emitting MQTT message for scheduled pass" "INFO"
+      PAYLOAD=$(jq -n \
+        --arg sat_name "$OBJ_NAME" \
+        --arg pass_start "$start_epoch_time" \
+        --arg pass_end "$end_epoch_time" \
+        --arg max_elev "$max_elev" \
+        --arg is_active "1" \
+        --arg pass_start_azimuth "$starting_azimuth" \
+        --arg azimuth_at_max "$azimuth_at_max" \
+        --arg direction "$direction" \
+        --arg at_job_id "$at_job_id" \
+        '{enable_mqtt: true, satellite: {name: $sat_name, pass_start: $pass_start, pass_end: $pass_end, max_elev: $max_elev, is_active: $is_active, pass_start_azimuth: $pass_start_azimuth, azimuth_at_max: $azimuth_at_max, direction: $direction, at_job_id: $at_job_id}}')
+      "$SCRIPTS_DIR/mqtt.sh" "iotstack/antenna/schedule" "$PAYLOAD"
     fi
   fi
 
