6846e98deeff24f47219b6ee1328a01c8cd50e62
Sacha <sacha.smart@gmail.com>
2025-03-09 12:35:07 -0400

    small fix

--- redrive-events.sh
+++ redrive-events.sh
@@ -9,21 +9,21 @@
 #     is_active boolean, pass_start_azimuth int, direction text, azimuth_at_max int, at_job_id int not null default 0);
 
 DB_FILE="/home/pi/raspberry-noaa-v2/db/panel.db"
+SCRIPTS_DIR="${NOAA_HOME}/scripts"
 
 
 scheduled_passes=$(sqlite3 $DB_FILE "SELECT * FROM predict_passes;")
-
 while IFS='|' read -r sat_name pass_start pass_end max_elev is_active pass_start_azimuth direction azimuth_at_max at_job_id; do
     PAYLOAD=$(jq -n \
-        --arg sat_name "$OBJ_NAME" \
-        --arg pass_start "$start_epoch_time" \
-        --arg pass_end "$end_epoch_time" \
+        --arg sat_name "$sat_name" \
+        --arg pass_start "$pass_start" \
+        --arg pass_end "$pass_end" \
         --arg max_elev "$max_elev" \
         --arg is_active "$is_active" \
-        --arg pass_start_azimuth "$starting_azimuth" \
+        --arg pass_start_azimuth "$pass_start_azimuth" \
         --arg azimuth_at_max "$azimuth_at_max" \
         --arg direction "$direction" \
         --arg at_job_id "$at_job_id" \
         '{enable_mqtt: true, satellite: {name: $sat_name, pass_start: $pass_start, pass_end: $pass_end, max_elev: $max_elev, is_active: $is_active, pass_start_azimuth: $pass_start_azimuth, azimuth_at_max: $azimuth_at_max, direction: $direction, at_job_id: $at_job_id}}')
     "$SCRIPTS_DIR/mqtt.sh" "iotstack/antenna/schedule" "$PAYLOAD"
-done <<< "$scheduled_passes"
+done <<< "$(echo "$scheduled_passes")"iotstack/antenna/schedule" "$PAYLOAD"
